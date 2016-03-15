6a4df2e68322619009f6cbebbdf8b3f8a5d9ed05
Philip Cutler <greenthegarden@gmail.com>
2016-03-15 12:27:53 +1030

    initial commit

---
+++ AutomationBoard.h
@@ -0,0 +1,22 @@
+#ifndef AUTOMATIONBOARD_H_
+#define AUTOMATIONBOARD_H_
+
+// For details about the Automation Board see
+// http://garagelab.com.br/automation/
+//  Digital I/O Pins: D0~D8 Reserved / D9~D13 Free
+// Analog Input Pins: A0 and A1 Reserved / A2~A5 Free
+
+// pin definitions for Automation Board
+
+#define RELAY_1      2  // Relay 1 is connected to Arduino D5
+#define RELAY_2      3  // Relay 2 is connected to Arduino D6
+#define RELAY_3      4  // Relay 3 is connected to Arduino D7
+#define RELAY_4      5  // Relay 4 is connected to Arduino D8
+
+//int Relays[8] = { RELAY_1, RELAY_2, RELAY_3, RELAY_4 };
+
+//int PWM_PINS[4] = { 2, 4, 5, 8 };
+
+
+
+#endif  /* AUTOMATIONBOARD_H_ */
---
+++ README.md
@@ -0,0 +1,6 @@
+# AutomationBoard
+Header file to specify the pin configuration of the Automation Board from GarageLab.
+
+See http://garagelab.com.br/automation/ for details about the Automation Board.
+
+Note: I do not have any association with GarageLab.
